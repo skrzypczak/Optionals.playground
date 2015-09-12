979834a1f4ccfe458d24896362c1ea38d253dc2d
Bill Skrzypczak <bill@webbuildersconsulting.com>
2015-09-12 12:30:02 -0500

    Optionals

---
+++ Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+//Optionals
+//
+//You use optionals in situations where a value may be absent. An optional says:
+//
+//There is a value, and it equals x
+//or
+//
+//There isn’t a value at all
+
+//If you define an optional variable without providing a default value, the variable is automatically set to nil for you:
+
+var surveyAnswer: String?
+
+// surveyAnswer is automatically set to nil
